5bd88b7f2476b423b22f3f8eac7628f248b5f141
Matt <38063055+Terracom12@users.noreply.github.com>
2024-04-26 16:43:12 -0700

    Remove conan.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include "storage/ExampleStorage.hpp"
 #include "ui/ExampleUI.hpp"
 #include <memory>
+#include <fmt/format.h>
 
 int main()
 {
+    fmt::println("{} {}", "world!", "Hello ");
+
     Application app(std::make_unique<ExampleStorage>(), std::make_unique<ExampleUI>());
 
     app.run();
