476986a3f8172eeff72d5824d72011dd81856210
Matthew Reese <awesomepop012@hotmail.com>
2024-04-26 12:39:26 -0700

    Restore simplified dataclass for Course.

---
+++ src/dataclasses.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+/**
+ * @brief Stores data pertaining to a community college course.
+ *
+ */
+struct Course
+{
+    std::string name;
+    // prerequisites
+    // units
+    // ...
+};
